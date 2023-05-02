50a004137ad5d2b4bc09bff9b7c3188a6d131f3e
dzxu <dongzexu@hotmail.com>
2023-05-02 19:43:35 +0800

    rider config file

--- Source/ActionRoguelike/ActionRoguelike.Build.cs
+++ Source/ActionRoguelike/ActionRoguelike.Build.cs
@@ -8,7 +8,13 @@ public class ActionRoguelike : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
